1276c575f54723b425687fafb0fa6d68fdb0a437
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2017-10-17 03:58:12 -0400

    Removed categorical crossentropy loss (buggy, untested).

--- lib/loss.py
+++ lib/loss.py
@@ -4,62 +4,6 @@ from keras import backend as K
 import numpy as np
 
 
-# Define axes according to keras.
-class_axis = 1 if K.image_data_format()=='channels_first' else -1
-
-
-def categorical_crossentropy(weighted=False, mask_class=None):
-    '''
-    Categorical crossentropy for N-dimensional inputs.
-    
-    Expects integer or one-hot class labeling in y_true.
-    
-    weighted : if True, loss is automatically reweighted with respect to
-        classes so that each class is given equal importance (simulates class
-        balancing).
-    mask_class : the (integer) class(es) that is/are masked out of the loss.
-    '''
-    if mask_class is not None and not hasattr(mask_class, '__len__'):
-        mask_class = [mask_class]
-    
-    def categorical_crossentropy(y_true, y_pred):
-        shape_y_pred_f = (K.prod(K.shape(y_pred)[:-1]), K.shape(y_pred)[-1])
-        y_pred_f = K.reshape(y_pred, shape_y_pred_f)
-        n_classes = y_pred.shape[1]
-        if K.ndim(y_true)==K.ndim(y_pred)-1:
-            y_true = K.one_hot(y_true, n_classes)
-            dim_order = [0, K.ndim(y_true)-1]+list(range(1, K.ndim(y_true)-1))
-            y_true = K.permute_dimensions(y_true, dim_order)
-        y_true_f = K.flatten(y_true)
-        y_true_f = K.cast(y_true_f, 'int32')
-        cce = K.categorical_crossentropy(y_pred_f, y_true_f)
-        if weighted:
-            # inverse proportion
-            non_class_axes = [i for i in range(K.ndim(y_true)) if i!=class_axis]
-            class_weights = K.sum(y_true) / K.sum(y_true,
-                                                  axis=non_class_axes,
-                                                  keepdims=True)
-            # weights sum to 1
-            class_weights = class_weights.flatten() / K.sum(class_weights)
-            weighted_y_true = y_true*class_weights
-            sample_weights = K.max(weighted_y_true, axis=class_axis)
-            wcce = cce*sample_weights
-        if mask_class is not None:
-            mask_out = K.sum([K.equal(y_true_f, t) for t in mask_class],
-                             axis=batch_axis)
-            idxs = K.not_equal(mask_out, 1).nonzero()
-            wcce = cce[idxs]
-        return K.mean(wcce)
-    
-    # Set a custom function name
-    tag = ""
-    if mask_class is not None:
-        tag += "_"+"_".join("m"+str(i) for i in mask_class)
-    dice.__name__ = "categorical_crossentropy"+tag
-    
-    return categorical_crossentropy
-
-
 def dice_loss(target_class=1, mask_class=None):
     '''
     Dice loss.

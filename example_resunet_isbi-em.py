de5c0480d3bfd90e02505b8488ed1f56cf680c7a
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2018-01-31 05:14:05 -0500

    Port example for pytorch.

--- example_resunet_isbi-em.py
+++ example_resunet_isbi-em.py
@@ -6,24 +6,30 @@ import sys
 
 import numpy as np
 import tifffile
-from keras import backend as K
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import (EarlyStopping, 
-                             LearningRateScheduler)
-from keras.optimizers import RMSprop
+import torch
+from torch.utils.data import DataLoader
 
+from torchsample import TensorDataset
+from torchsample.modules import ModuleTrainer
+from torchsample.regularizers import L2Regularizer
+from torchsample.transforms import (RandomAffine,
+                                    RandomFlip,
+                                    Compose)
+from torchsample.callbacks import (EarlyStopping, 
+                                   LRScheduler)
+from torchsample.metrics import BinaryAccuracy
+                                   
 from fcn_maker.model import assemble_resunet
 from fcn_maker.loss import dice_loss
-from fcn_maker.blocks import (basic_block_mp,
+from fcn_maker.blocks import (tiny_block,
                               basic_block,
-                              bottleneck,
-                              get_channel_axis)
+                              bottleneck)
 
 '''
 Settings.
 '''
 model_kwargs = OrderedDict((
-    ('input_shape', None),                      # Filled in __main__
+    ('in_channels', 1),                     # Filled in __main__
     ('num_classes', 1),
     ('num_init_blocks', 2),
     ('num_main_blocks', 3),
@@ -33,18 +39,18 @@ model_kwargs = OrderedDict((
     ('long_skip', True),
     ('long_skip_merge_mode', 'sum'),
     ('main_block', bottleneck),
-    ('init_block', basic_block_mp),
+    ('init_block', tiny_block),
     ('upsample_mode', 'repeat'),
     ('dropout', 0.1),
-    # ('normalization': None),                  # Default is BatchNormalization
-    # ('norm_kwargs': None),                    # Already set by default
-    ('weight_decay', 0.0001), 
-    ('init', 'he_normal'),
-    ('nonlinearity', 'relu'),
+    # ('normalization', None),                 # Default is batch_normalization
+    # ('norm_kwargs', None),                   # Already set by default
+    ('init', 'kaiming_normal'),
+    ('nonlinearity', 'ReLU'),
     ('ndim', 2),
-    ('verbose', True)                           # Set False to silence stdout
+    ('verbose', True)                          # Set False to silence stdout
     ))
-batch_size = 1
+batch_size = 4
+cuda_device = 0
 
 
 '''
@@ -56,51 +62,24 @@ ds_path = {'train-volume': data_path + "train-volume.tif",
            'test-volume': data_path + "test-volume.tif"}
 
 
-'''
-Learning rate scheduler.
-'''
-def scheduler(epoch):
-    if epoch%200==0 and epoch>0:
-        tmp_lr = (K.get_value(model.optimizer.lr)/10).astype('float32')
-        K.set_value(model.optimizer.lr, tmp_lr)
-    lr = K.get_value(model.optimizer.lr)
-    return np.float(lr)
-
-'''
-Accuracy metric.
-'''
-def accuracy(y_true, y_pred):
-    if model_kwargs['num_classes']==1:
-        return K.mean(K.equal(y_true, K.round(y_pred)))
-    else:
-        return K.mean(K.equal(K.squeeze(y_true, 1),
-                              K.argmax(y_pred, axis=-1)))
+#'''
+#Learning rate scheduler.
+#'''
+#def scheduler(epoch, logs=None):
+    #if epoch%200==0 and epoch>0:
+        #tmp_lr = (K.get_value(model.optimizer.lr)/10).astype('float32')
+        #K.set_value(model.optimizer.lr, tmp_lr)
+    #lr = K.get_value(model.optimizer.lr)
+    #return np.float(lr)
 
 
 if __name__=='__main__':
-    '''
-    Default recursion limit is too low for resnets.
-    '''
-    sys.setrecursionlimit(99999)
-
-    '''
-    Handle input shape for channels_first vs channels_last.
-    (eg. theano vs tensorflow)
-    '''
-    ndim = 2
-    channel_axis = get_channel_axis(ndim)
-    if channel_axis==1:
-        input_shape = (1, 512, 512)
-    else:
-        input_shape = (512, 512, 1)
-    model_kwargs['input_shape'] = input_shape
-        
     '''
     Prepare data -- load, standardize, add channel dim., shuffle, split.
     '''
     # Load
     X = tifffile.imread(ds_path['train-volume']).astype(np.float32)
-    Y = tifffile.imread(ds_path['train-labels']).astype(np.int32)
+    Y = tifffile.imread(ds_path['train-labels']).astype(np.int64)
     Y[Y==255] = 1
     # Standardize each sample individually (mean center, variance normalize)
     mean = X.reshape(X.shape[0],
@@ -109,8 +88,8 @@ if __name__=='__main__':
                      np.prod(X.shape[1:])).std(axis=-1)[:,None,None]
     X = (X-mean)/std
     # Add channel dim
-    X = np.expand_dims(X, axis=channel_axis)
-    Y = np.expand_dims(Y, axis=channel_axis)
+    X = np.expand_dims(X, axis=1)
+    Y = np.expand_dims(Y, axis=1)
     # Shuffle
     R = np.random.permutation(len(X))
     X = X[R]
@@ -119,44 +98,46 @@ if __name__=='__main__':
     # Split (26 training, 4 validation)
     X_train = X[:26]
     Y_train = Y[:26]
-    X_val = X[26:]
-    Y_val = Y[26:]
-    # Prepare data augmentation
-    datagen = ImageDataGenerator(rotation_range=25,
-                                 shear_range=0.41,
-                                 horizontal_flip=True,
-                                 vertical_flip=True,
-                                 fill_mode='reflect')
+    X_valid = X[26:]
+    Y_valid = Y[26:]
+    # Prepare data augmentation and data loaders.
+    transform = Compose([RandomAffine(rotation_range=25, shear_range=0.41),
+                         RandomFlip(h=True, v=True)])
+    ds_train = TensorDataset(X_train, Y_train,
+                             co_transform=transform)
+    ds_valid = TensorDataset(X_valid, Y_valid)
+    loader_train = DataLoader(ds_train, batch_size=batch_size, shuffle=True)
+    loader_valid = DataLoader(ds_valid, batch_size=batch_size)
     
     '''
     Prepare model.
     '''
     model = assemble_resunet(**model_kwargs)
-    optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08)
-    model.compile(loss=dice_loss(), 
-                  optimizer=optimizer, 
-                  metrics=[accuracy])
-
+    model.cuda(cuda_device)
+    optimizer = torch.optim.RMSprop(params=model.parameters(),
+                                    lr=0.001, alpha=0.9)
+    trainer = ModuleTrainer(model)
+    l2_reg = L2Regularizer(scale=1e-4, module_filter='*conv*')
+    trainer.set_regularizers(l2_reg)
+    trainer.compile(loss=dice_loss(),
+                    optimizer=optimizer,
+                    metrics=[BinaryAccuracy()])
+    
+    
     '''
     Prepare callbacks.
     '''
     callbacks=[]
-    callbacks.append( LearningRateScheduler(scheduler) )
+    #callbacks.append( LRScheduler(scheduler) )
     callbacks.append( EarlyStopping(monitor='val_loss', 
                                     patience=200,
-                                    verbose=0,
-                                    mode='auto') )
+                                    min_delta=0) )
     
     '''
     Train the model.
     '''
-    model.fit_generator(datagen.flow(X_train, 
-                                     Y_train, 
-                                     batch_size=batch_size, 
-                                     shuffle=True),
-                        steps_per_epoch=len(X_train)//batch_size, 
-                        epochs=500,
-                        callbacks=callbacks,
-                        validation_data=(X_val, Y_val),
-                        verbose=2)
-    
+    trainer.fit_loader(loader=loader_train,
+                       val_loader=loader_valid,
+                       num_epoch=500,
+                       verbose=1,
+                       cuda_device=cuda_device)    
